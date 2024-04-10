c761793eeeba6c04bac2fb5e7ac86c629a36f565
Job Shibu <jobshibukulathunkal99@gmail.com>
2024-04-10 18:45:06 +0530

    simplilearn 5.1 20%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import FunctionalComp from './components/FunctionalComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React app
-        </a>
-      </header>
+    <div class="parent">
+      <p>first edit :)</p>
+      <div> this is a div </div>
+      <FunctionalComp/>
     </div>
   );
 }
---
+++ src/components/FunctionalComp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function FunctionalComp(){
+    return <p>FunctionalComp</p>
+}
+
+export default FunctionalComp;
